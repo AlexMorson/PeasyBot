464ef2a6e26af1a500733c8ac13dcc5fd966a7c9
Pythonese <alexmorson@btinternet.com>
2016-12-15 23:59:41 +0000

    Added Condition and Result

--- bot.py
+++ bot.py
@@ -2,31 +2,38 @@ from enum import Enum
 from discord import Client
 client = Client()
 
-class CommandType(Enum):
-    NoResult = 0
-    SendMessage = 1
-    CreateRoom = 2
+class Condition:
+    def __init__(self, start=None, author=None, channel=None):
+        self.start = start
+        self.author = author
+        self.channel = channel
+
+    def holds(self, message):
+        return (self.start   is None or message.content.startswith(self.start)) and\
+               (self.author  is None or message.author  == self.author        ) and\
+               (self.channel is None or message.channel == self.channel       )
+
+class Result:
+    def __init__(self, sendMessage=None, createChannel=None):
+        self.sendMessage = sendMessage
+        self.createChannel = createChannel
+
+    async def doActions(self, message):
+        if self.sendMessage is not None:
+            await client.send_message(message.channel, self.sendMessage)
+        if self.createChannel is not None:
+            await client.create_channel(message.server, self.createChannel)
 
 class Command:
-    def __init__(self, func, commandTypes, start, channel, server): #commandTypes is a list
+    def __init__(self, func, condition):
         self.func = func
-        self.commandTypes = commandTypes
-        self.start = start
-        self.channel = channel
-        self.server = server
+        self.condition = condition
         
     async def __call__(self, message):
-        if (self.start   is None or message.content.startswith(self.start)) and\
-           (self.channel is None or message.channel == self.channel       ) and\
-           (self.server  is None or message.server  == self.server        ):
-            output = self.func(message)
-            for commandType in self.commandTypes:
-                if commandType is CommandType.NoResult:
-                    break
-                if commandType is CommandType.SendMessage:
-                    await client.send_message(message.channel, output.pop(0))
-                if commandType is CommandType.CreateRoom:
-                    await client.create_channel(message.server, output.pop(0))
+        if self.condition.holds(message):
+            result = self.func(message)
+            if result is not None:
+                await result.doActions(message)
 
 @client.event
 async def on_ready():
@@ -38,9 +45,9 @@ async def on_message(message):
     for command in commands:
         await command(message)
 
-def command(commandType, start=None, channel=None, server=None):
+def command(condition):
     def decorator(func):
-        commands.append(Command(func, commandType, start, channel, server))
+        commands.append(Command(func, condition))
     return decorator
 
 def run(token):
--- peasyBot.py
+++ peasyBot.py
@@ -1,8 +1,12 @@
 import bot
 
-@bot.command([bot.CommandType.SendMessage, bot.CommandType.CreateRoom], "!create")
+@bot.command(bot.Condition(start="!create"))
 def createRoom(message):
     channelName = "".join(message.content[len("!create"):].split()).lower()
-    return ["Creating channel called {}.".format(channelName), channelName]
+    return bot.Result(sendMessage="Creating channel called {}.".format(channelName), createChannel=channelName)
+
+@bot.command(bot.Condition(start="!echo"))
+def echo(message):
+    return bot.Result(sendMessage=message.content[len("!echo"):].lstrip())
 
 bot.run("MjUyMTk1MjYxMTM5Mzg2MzY5.Cxur_Q.QDEkWgWeEHyTHNVlog0hCVwDmM0")
